2fa7e8337009c49a511f1b6ad7eb5b54c7e904ef
bnevr <51931595+bnevr@users.noreply.github.com>
2023-05-13 18:18:03 +0000

    fix path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashComponent } from './dash/dash.component';
 
-const routes: Routes = [{ path: 'dashboard', component: DashComponent }];
+const routes: Routes = [{ path: '', component: DashComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
